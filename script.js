26375a152be286f940cc4c8d80cf9da83792d2ec
arsanjeevk <arsanjeevk@gmail.com>
2026-06-01 15:52:58 +0530

    new websit

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
-//hamburger btn navigation bar
+var menubtn = document.getElementById("menubtn");
+var sidenav = document.getElementById("sidenav");
+var menu = document.getElementById("menu");
 
-// $("#toggle").click(function () {
-
-//     $(this).toggleClass('on');
-//     $("#resize").toggleClass("active");
-
-// });
-
-
-var menubtn = document.getElementById("menubtn")
-var sidenav = document.getElementById("sidenav")
-var menu = document.getElementById("menu")
 sidenav.style.right = "-100vw";
-menubtn.onclick = function() {
-  if (sidenav.style.right == "-100vw") {
-    sidenav.style.right = "0";
-    menu.src = "close";
-  }
-  else {
-    sidenav.style.right = "-100vw";
-    menu.src = "menu";
-  }
-}
 
-// online offline deduction
-function myFunction() {
-  var x = "Is the browser online? " + navigator.onLine;
-  document.getElementById("demo").innerHTML = x;
-}
+menubtn.onclick = function () {
+    if (sidenav.style.right === "-100vw") {
+        sidenav.style.right = "0";
+        menu.textContent = "Close";
+    } else {
+        sidenav.style.right = "-100vw";
+        menu.textContent = "Menu";
+    }
+};
 
-
-
-
-
+document.querySelectorAll("#sidenav a").forEach(link => {
+    link.addEventListener("click", function () {
+        sidenav.style.right = "-100vw";
+        menu.textContent = "Menu";
+    });
+});
